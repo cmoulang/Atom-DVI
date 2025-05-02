aef2c481f70fd343fc78d17e1efb39bc6c538d90
Chris Moulang <chris@moulang.org>
2025-05-02 17:24:42 +0100

    Added GPL3 license text

---
+++ licence.h
@@ -0,0 +1,21 @@
+const char gpl3_notice[] =
+    "ATOM-DVI V0.0.4-beta\n\n"
+
+
+    "Copyright 2025 Chris Moulang\n"
+    "Contributors: Phill Harvey-Smith and David Banks\n"
+    "VGA mode added by David Banks\n"
+    "SID emulation uses code from reSID by Dag Lem\n\n"
+
+    "ATOM DVI is free software: you can redistribute it and/or modify\n"
+    "it under the terms of the GNU General Public License as published by\n"
+    "the Free Software Foundation, either version 3 of the License, or\n"
+    "(at your option) any later version.\n"
+    "\n"
+    "This program is distributed in the hope that it will be useful,\n"
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+    "GNU General Public License for more details.\n"
+    "\n"
+    "You should have received a copy of the GNU General Public License\n"
+    "along with this program.  If not, see <https://www.gnu.org/licenses/>.\n";
