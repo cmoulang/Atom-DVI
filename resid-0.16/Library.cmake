23f4ff54071603f929a8f0c64aa22d80590d1cbe
Chris Moulang <chris@moulang.org>
2025-05-15 12:26:27 +0100

    move resid source file list to separate include file

---
+++ resid-0.16/Library.cmake
@@ -0,0 +1,17 @@
+list(APPEND SOURCE_FILES
+    resid-0.16/envelope.cc
+    resid-0.16/extfilt.cc
+    resid-0.16/pot.cc
+    resid-0.16/filter.cc
+    resid-0.16/sid.cc
+    resid-0.16/voice.cc
+    resid-0.16/wave.cc
+    resid-0.16/wave6581__ST.cc
+    resid-0.16/wave6581_P_T.cc
+    resid-0.16/wave6581_PS_.cc
+    resid-0.16/wave6581_PST.cc
+    resid-0.16/wave8580__ST.cc
+    resid-0.16/wave8580_P_T.cc
+    resid-0.16/wave8580_PS_.cc
+    resid-0.16/wave8580_PST.cc      
+)
