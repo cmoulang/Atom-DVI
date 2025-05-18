8fb9c476815be0c073a572f47ea8a489058aa0b9
Chris Moulang <chris@moulang.org>
2025-05-18 10:43:12 +0100

    generate a 1x1 pixel png file when user presses button

--- capture.c
+++ capture.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 #include "ff.h"
-#include "tusb.h"
 #include "png.h"
+#include "tusb.h"
 
 #define DNAME "/atom"
 #define FILE_PREFIX "scr"
@@ -39,106 +39,136 @@ int last_file_number(char* dname) {
     }
 }
 
-void setRGB(png_byte *ptr, float val)
-{
-	int v = (int)(val * 767);
-	if (v < 0) v = 0;
-	if (v > 767) v = 767;
-	int offset = v % 256;
+void setRGB(png_byte* ptr, float val) {
+    int v = (int)(val * 767);
+    if (v < 0) v = 0;
+    if (v > 767) v = 767;
+    int offset = v % 256;
 
-	if (v<256) {
-		ptr[0] = 0; ptr[1] = 0; ptr[2] = offset;
-	}
-	else if (v<512) {
-		ptr[0] = 0; ptr[1] = offset; ptr[2] = 255-offset;
-	}
-	else {
-		ptr[0] = offset; ptr[1] = 255-offset; ptr[2] = 0;
-	}
+    if (v < 256) {
+        ptr[0] = 0;
+        ptr[1] = 0;
+        ptr[2] = offset;
+    } else if (v < 512) {
+        ptr[0] = 0;
+        ptr[1] = offset;
+        ptr[2] = 255 - offset;
+    } else {
+        ptr[0] = offset;
+        ptr[1] = 255 - offset;
+        ptr[2] = 0;
+    }
 }
 
-
-int writeImage(char* filename, int width, int height, float *buffer, char* title)
-{
-	int code = 0;
-	FILE *fp = NULL;
-	png_structp png_ptr = NULL;
-	png_infop info_ptr = NULL;
-	png_bytep row = NULL;
-	
-	// Open file for writing (binary mode)
-	fp = fopen(filename, "wb");
-	if (fp == NULL) {
-		fprintf(stderr, "Could not open file %s for writing\n", filename);
-		code = 1;
-		goto finalise;
-	}
-
-	// Initialize write structure
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (png_ptr == NULL) {
-		fprintf(stderr, "Could not allocate write struct\n");
-		code = 1;
-		goto finalise;
-	}
-
-	// Initialize info structure
-	info_ptr = png_create_info_struct(png_ptr);
-	if (info_ptr == NULL) {
-		fprintf(stderr, "Could not allocate info struct\n");
-		code = 1;
-		goto finalise;
-	}
-
-	// Setup Exception handling
-	if (setjmp(png_jmpbuf(png_ptr))) {
-		fprintf(stderr, "Error during png creation\n");
-		code = 1;
-		goto finalise;
-	}
-
-	png_init_io(png_ptr, fp);
-
-	// Write header (8 bit colour depth)
-	png_set_IHDR(png_ptr, info_ptr, width, height,
-			8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-			PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-	// Set title
-	if (title != NULL) {
-		png_text title_text;
-		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
-		title_text.key = "Title";
-		title_text.text = title;
-		png_set_text(png_ptr, info_ptr, &title_text, 1);
-	}
-
-	png_write_info(png_ptr, info_ptr);
-
-	// Allocate memory for one row (3 bytes per pixel - RGB)
-	row = (png_bytep) malloc(3 * width * sizeof(png_byte));
-
-	// Write image data
-	int x, y;
-	for (y=0 ; y<height ; y++) {
-		for (x=0 ; x<width ; x++) {
-			setRGB(&(row[x*3]), buffer[y*width + x]);
-		}
-		png_write_row(png_ptr, row);
-	}
-
-	// End write
-	png_write_end(png_ptr, NULL);
-
-	finalise:
-	if (fp != NULL) fclose(fp);
-	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
-	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-	if (row != NULL) free(row);
-
-	return code;
+void user_read_data(png_structp png_ptr, png_bytep data, size_t length) {
+    printf("user_read_data\n");
 }
 
+void user_write_data(png_structp png_ptr, png_bytep data, size_t length) {
+    printf("user_write_data %d\n", length);
+    int offset = 0;
+    while (length > 0) {
+        int bw;
+        FRESULT result = f_write(png_get_io_ptr(png_ptr), data+offset, length, &bw);
+        if (result != FR_OK) {
+            printf("f_write failed %d\n", result);
+            return;
+        }
+        length -= bw;
+        offset += bw;
+    }
+}
+
+void user_flush_data(png_structp png_ptr) { printf("user_flush_data\n"); }
+
+int writeImage(char* filename, int width, int height, float* buffer,
+               char* title) {
+    int code = 0;
+    FILE* fp = NULL;
+    png_structp png_ptr = NULL;
+    png_infop info_ptr = NULL;
+    png_bytep row = NULL;
+
+    // Open file for writing (binary mode)
+    FIL file;
+    if (FR_OK != f_open(&file, filename, FA_CREATE_NEW | FA_WRITE)) {
+        fp = NULL;
+    } else {
+        fp = (FILE*)&file;
+    }
+    if (fp == NULL) {
+        fprintf(stderr, "Could not open file %s for writing\n", filename);
+        code = 1;
+        goto finalise;
+    }
+
+    // Initialize write structure
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (png_ptr == NULL) {
+        fprintf(stderr, "Could not allocate write struct\n");
+        code = 1;
+        goto finalise;
+    }
+
+    // Initialize info structure
+    info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL) {
+        fprintf(stderr, "Could not allocate info struct\n");
+        code = 1;
+        goto finalise;
+    }
+
+    // Setup Exception handling
+    if (setjmp(png_jmpbuf(png_ptr))) {
+        fprintf(stderr, "Error during png creation\n");
+        code = 1;
+        goto finalise;
+    }
+
+    png_set_write_fn(png_ptr, fp, user_write_data, user_flush_data);
+    png_set_compression_mem_level(png_ptr, 4);
+
+    // png_init_io(png_ptr, fp);
+
+    // Write header (8 bit colour depth)
+    png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB,
+                 PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE,
+                 PNG_FILTER_TYPE_BASE);
+
+    // Set title
+    if (title != NULL) {
+        png_text title_text;
+        title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+        title_text.key = "Title";
+        title_text.text = title;
+        png_set_text(png_ptr, info_ptr, &title_text, 1);
+    }
+
+    png_write_info(png_ptr, info_ptr);
+
+    // Allocate memory for one row (3 bytes per pixel - RGB)
+    row = (png_bytep)malloc(3 * width * sizeof(png_byte));
+
+    // Write image data
+    int x, y;
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            setRGB(&(row[x * 3]), buffer[y * width + x]);
+        }
+        png_write_row(png_ptr, row);
+    }
+
+    // End write
+    png_write_end(png_ptr, NULL);
+
+finalise:
+    if (fp != NULL) f_close((FIL*)fp);
+    if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+    if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+    if (row != NULL) free(row);
+
+    return code;
+}
 
 bool ls(const char* dpath) {
     DIR dir;
@@ -171,6 +201,8 @@ bool ls(const char* dpath) {
 
 void capture_task() { tuh_task(); }
 
+png_structp write_ptr;
+
 void capture_init() {
     int status = tuh_init(BOARD_TUH_RHPORT);
     printf("status = %d\n", status);
@@ -193,8 +225,7 @@ void capture() {
     printf("fname = %s\n", fname);
 
     float buffer[1];
-    writeImage(fname, 1,1,buffer,"Hello");
-
+    writeImage(fname, 1, 1, buffer, "Hello");
 
     // FIL file;
     // int status = f_open(&file, fname, FA_CREATE_NEW | FA_WRITE);
